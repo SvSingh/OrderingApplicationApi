1a1952d597fa38d8afe55626e082ee9fbf786340
SV Singh <svsingh@SVs-MacBook-Air.local>
2022-04-15 23:31:24 -0700

    added jwt Token authorization

--- src/main/java/com/SvTech/SpringApi/Security/SecurityConfig.java
+++ src/main/java/com/SvTech/SpringApi/Security/SecurityConfig.java
@@ -6,11 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -24,32 +26,48 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired private JWTFilter filter;
     @Autowired private MyUserDEtailService uds;
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .httpBasic().disable()
-                .cors()
-                .and()
-                .authorizeHttpRequests()
-                .antMatchers("/api/auth/**").permitAll()
-                .antMatchers("/api/user/**").hasRole("USER")
-                .and()
-                .userDetailsService(uds)
-                .exceptionHandling()
-                .authenticationEntryPoint(
-                        (request, response, authException) ->
-                                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized")
-                )
-                .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
-        http.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(uds);
+    }
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf().disable()
+                .authorizeRequests().antMatchers("/api/auth/**").permitAll().
+                anyRequest().authenticated().and().
+                exceptionHandling().and().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        httpSecurity.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
+
     }
 
+    /*@Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.csrf().disable()
+                    .httpBasic().disable()
+                    .cors()
+                    .and()
+                    .authorizeHttpRequests()
+                    .antMatchers("/api/auth/**").permitAll()
+                    .antMatchers("/api/user/**").hasRole("USER")
+                    .and()
+                    .userDetailsService(uds)
+                    .exceptionHandling()
+                    .authenticationEntryPoint(
+                            (request, response, authException) ->
+                                    response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized")
+                    )
+                    .and()
+                    .sessionManagement()
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+            http.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
+        }
+    */
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return NoOpPasswordEncoder.getInstance();
     }
 
     @Bean
